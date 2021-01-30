38ba20674cae4b023fdf9b6034625390f39a9e0e
Victor Hugo <victor.furtado@alu.ufc.br>
2021-01-30 15:56:09 -0300

    Gramar adjust

--- src/data/substances.js
+++ src/data/substances.js
@@ -1,6 +1,6 @@
 export default [
     { text: 'Álcool', id: '0', value: '0', opt: ['lata','litro','taça','dose'] },
-    { text: 'Bala', id: '1', value: '0', opt: ['miligramas','balas'] },
-    { text: 'Café', id: '2', value: '0', opt: ['xícara', 'garrafa','litros'] },
-    { text: 'Cigarro', id: '3', value: '0', opt: ['cigarro','maço','gramas'] },
+    { text: 'Bala', id: '1', value: '0', opt: ['miligrama','bala'] },
+    { text: 'Café', id: '2', value: '0', opt: ['xícara', 'garrafa','litro'] },
+    { text: 'Cigarro', id: '3', value: '0', opt: ['cigarro','maço','grama'] },
 ]
