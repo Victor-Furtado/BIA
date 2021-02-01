8bd0e89135f351eeccf5d50cea62eb20ca88ecdd
Victor Hugo <victor.furtado@alu.ufc.br>
2021-02-01 17:14:59 -0300

    MinorFix

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -7,7 +7,7 @@ export default ({ navigation }) => {
     return (
         <View style={{ margin: 20 }}>
             <FlatBtn label='Nova Entrada no Diário' icon='+' onPress={_ => navigation.navigate('NewEntry')} />
-            <Calendar />
+            <Calendar onPress={console.log} />
             <FlatBtn clear label='Ver estatísticas' icon='&#x1F4CA;' />
         </View>
     )
